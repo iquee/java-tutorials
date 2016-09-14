f24cfa2242a57afdb7ea3405721e4e6567e75dbb
luiz.taira@fluig.com <luiz.taira@fluig.com>
2016-09-14 14:58:22 -0300

    2 novos testes: Loop e tipos primitivos

---
+++ src/main/java/br/com/luiztaira/introduction/TestLoop2.java
@@ -0,0 +1,28 @@
+package br.com.luiztaira.introduction;
+
+import java.util.Scanner;
+
+public class TestLoop2 {
+    public static void main(String[] argh) {
+        /**
+        First number, is the iteration
+        Given 3 input number(a, b, n), calculate:
+        (a+2^0*b), (a+2^1*b),...,(a+2^0*b + a+2^1*b + ... + a+2^n-1*b)
+        */
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int s = a;
+            for (int j = 0; j < n; ++j) {
+                s += b * ((int) (Math.pow(2, j)));
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
