d7de2b654bd4c8f4c27533e3e0010cf66482445f
Luiz Henrique K Taira <luiz.taira@fluig.com>
2017-08-10 18:29:59 -0300

    callable

---
+++ src/main/java/br/com/luiztaira/threads/module6/ThreadsCallable.java
@@ -0,0 +1,14 @@
+package br.com.luiztaira.threads.module6;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Created by taira on 8/10/17.
+ */
+public class ThreadsCallable implements Callable<String>{
+
+    @Override
+    public String call() throws Exception {
+        return null;
+    }
+}
