131394f397a9311d9b6dcf8dfc12724a2a6279ef
Luiz Henrique Konishi Taira <luiz.taira@fluig.com>
2017-09-27 14:15:18 -0300

    correcoes de pacote

--- src/main/java/br/com/luiztaira/threads/module5/TaskServer.java
+++ src/main/java/br/com/luiztaira/threads/module5/TaskServer.java
@@ -1,10 +1,7 @@
-
-
-import br.com.luiztaira.threads.module5.TaskDistributor;
+package br.com.luiztaira.threads.module5;
 
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,24 +10,23 @@ import java.util.concurrent.Executors;
  */
 public class TaskServer {
 
-    private static int number = 0;
+	private static int number = 0;
 
-    public static void main(String[] args)  throws Exception{
+	public static void main(String[] args) throws Exception {
 
-        System.out.println("Starting server....");
-        ServerSocket server = new ServerSocket(12345);
-        // newCachedThreadPool increase and descrease automatic after 60 sec
-        ExecutorService threadPool = Executors.newCachedThreadPool();
+		System.out.println("Starting server....");
+		ServerSocket server = new ServerSocket(12345);
+		// newCachedThreadPool increase and descrease automatic after 60 sec
+		ExecutorService threadPool = Executors.newCachedThreadPool();
 
+		System.out.println("Waiting connections....");
+		while (true) {
+			Socket socket = server.accept();
+			System.out.println("    Connection accepted: port -> " + socket.getPort());
 
-        System.out.println("Waiting connections....");
-        while (true){
-            Socket socket = server.accept();
-            System.out.println("    Connection accepted: port -> " + socket.getPort());
-
-            TaskDistributor taskDistributor = new TaskDistributor(socket, ++number);
-            threadPool.execute(taskDistributor);
-        }
-    }
+			TaskDistributor taskDistributor = new TaskDistributor(socket, ++number);
+			threadPool.execute(taskDistributor);
+		}
+	}
 
 }
