176506e66a5070a11b89c0c93e5aa641c65be258
Luiz Henrique K Taira <luizcomz@gmail.com>
2018-03-16 01:06:19 -0300

    novos algoritimos

--- src/main/java/br/com/luiztaira/algorithms/BinarySearch.java
+++ src/main/java/br/com/luiztaira/algorithms/BinarySearch.java
@@ -1,4 +1,35 @@
 package br.com.luiztaira.algorithms;
 
 public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] array ={1,5,6,3,-9,20};
+        Algorithm a = new Algorithm(array);
+        System.out.println(a.bynarySearch(0, array.length, 3));
+    }
+}
+
+
+class Algorithm{
+
+    private int[] array;
+
+    public Algorithm(int[] array){
+        this.array = array;
+    }
+
+    public int bynarySearch(int startIndex, int endIndex, int item){
+        if(endIndex < startIndex)
+            return -1;
+
+        int middleIndex = (startIndex + endIndex) / 2;
+        if(item == array[middleIndex])
+            return middleIndex;
+        else if(item < array[middleIndex])
+            return bynarySearch(startIndex, middleIndex - 1, item);
+        else
+            return bynarySearch(middleIndex + 1, endIndex, item);
+    }
+
 }
--- src/main/java/br/com/luiztaira/algorithms/GreatestCommonDivisor.java
+++ src/main/java/br/com/luiztaira/algorithms/GreatestCommonDivisor.java
@@ -1,8 +1,26 @@
 package br.com.luiztaira.algorithms;
 
+/**
+ * A method to find the greatest common divisor - GCD
+ * of two numbers.
+ *
+ */
 public class GreatestCommonDivisor {
 
     public static void main(String[] args) {
-        
+        System.out.println(gcdRecursive(9,3));
+    }
+
+    /**
+     * A recursive calls, until the n2 becomes 0
+     *
+     * @param n1
+     * @param n2
+     * @return
+     */
+    public static int gcdRecursive(int n1, int n2){
+        if(n2 == 0)
+            return n1;
+        return gcdRecursive(n2, n1%n2);
     }
 }
